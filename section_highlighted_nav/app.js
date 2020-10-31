541e633025755d6d7b5ce701528104cfe7f2c778
ulfat-dawood <ulfat_dawood@outlook.com>
2020-10-31 15:20:44 +0400

    f

---
+++ section_highlighted_nav/app.js
@@ -0,0 +1,28 @@
+// const sections= document.querySelector('section');
+// const bubble= document.querySelector('.bubble');
+// console.log("hi"); 
+
+
+// const gradients= [
+//     'linear-gradient(to right, #d9a7c7, #fffcdc)',
+//     'linear-gradient(to right, #dd5e89, #f7bb97)',
+//     'linear-gradient(to right, #2bc0e4, #eaecc6)'
+
+// ]; 
+
+// const options= {
+//     threshold: 0.7
+// }
+
+// let observer= new IntersectionObserver(navcheck, options); 
+
+// function navcheck(entries){
+//     entries.forEach(entry => {
+//         console.log(entry);
+//     })
+
+// }
+
+// sections.forEach(section =>{
+//     observer.observe(section); 
+// })
