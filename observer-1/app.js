d023d0807734dcc9cfadd0ef8046fb446a5bc158
ulfat-dawood <ulfat_dawood@outlook.com>
2021-01-14 03:57:13 +0300

    is intersectiong

--- observer-1/app.js
+++ observer-1/app.js
@@ -1,14 +1,21 @@
-const section1 = document.querySelector("#section-1"); 
+const section1 = document.querySelector("#section-2"); 
 
 const callback = (entries, observer)=>{
     entries.forEach(entry => {
-        console.log(entry.target);
+        console.log(entry.target.id, "is intersecting? ", entry.isIntersecting);
+        if(entry.isIntersecting==true){
+            section1.classList.add("inverse")
+        }else if(entry.isIntersecting==false){
+            section1.classList.remove("inverse")
+        }
     });
 } 
 const options = {
     //root is by def the  viewport, so don't change it
     root: null, 
-    threshold: 0, // 0-1 where .5 fires the entry when it is 50% in the viewport
+    threshold: .5, // 0-1 where 
+                    //.25=> fires the entry ONLY when 25% of it is in the viewport
+                    //  1=> fires the entry ONLY when 100% of it is in the viewport
     rootMargin: "0px" //px or % string value, clockwise or shorthand
                   //rootMargin creates an imaginary viewport that is 
                   //------smaller than the actual viewport if margin is negative, 
