6ac613f92eccbd4cb0d419083f5a1f65454ba759
ulfat-dawood <ulfat_dawood@outlook.com>
2021-01-14 04:20:14 +0300

    final

--- observer-1/app.js
+++ observer-1/app.js
@@ -1,17 +1,20 @@
-const section1 = document.querySelector("#section-2"); 
+const section1 = document.querySelector("#section-1"); 
+const section2 = document.querySelector("#section-2"); 
+const section3 = document.querySelector("#section-3"); 
+const section4 = document.querySelector("#section-4"); 
 
 const callback = (entries, observer)=>{
     entries.forEach(entry => {
-        console.log(entry.target.id, "is intersecting? ", entry.isIntersecting);
         if(entry.isIntersecting==true){
-            section1.classList.add("inverse")
-        }else if(entry.isIntersecting==false){
-            section1.classList.remove("inverse")
+
+            console.log(entry.target.id, "is intersecting? ", entry.isIntersecting);
+            entry.target.classList.add("inverse")
+            observer.unobserve(entry.target); 
         }
     });
 } 
 const options = {
-    //root is by def the  viewport, so don't change it
+    //root is by default the viewport, so don't change it
     root: null, 
     threshold: .5, // 0-1 where 
                     //.25=> fires the entry ONLY when 25% of it is in the viewport
@@ -23,4 +26,8 @@ const options = {
 };
 const observer = new IntersectionObserver(callback, options);
 
-observer.observe(section1); //section is now observed when intersection and not intersecting with the viewport
+//each section is being observed all the time, reporting whether or not it is intersection with the viewport threshold.
+observer.observe(section1); 
+observer.observe(section2); 
+observer.observe(section3); 
+observer.observe(section4); 
